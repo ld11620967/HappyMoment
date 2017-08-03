721b139f81206e8c239436edf54b257afeff9eb5
ld11620967 <11620967@qq.com>
2017-08-04 01:00:07 +0800

    Optimize Glide V4

--- app/src/main/java/com/nilin/happymoment/ui/adapter/PictureAdapter.kt
+++ app/src/main/java/com/nilin/happymoment/ui/adapter/PictureAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.text.format.DateUtils
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.nilin.happymoment.bean.Contentlist
 import java.text.SimpleDateFormat
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 
 
 class PictureAdapter(var context: Context, layoutId:Int) : BaseQuickAdapter<Contentlist, BaseViewHolder>(layoutId) {
@@ -21,9 +24,17 @@ class PictureAdapter(var context: Context, layoutId:Int) : BaseQuickAdapter<Cont
 
         val image: ImageView = viewHolder.getView<ImageView>(R.id.image)
 
+        val options = RequestOptions()
+                .centerCrop()
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.error)
+                .priority(Priority.HIGH)
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
         Glide
                 .with(context)
                 .load(article.image0)
+                .apply(options)
+                .transition(DrawableTransitionOptions.withCrossFade(50))
                 .into(image)
     }
 }
